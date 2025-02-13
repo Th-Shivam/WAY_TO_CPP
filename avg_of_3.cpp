46c90aa4a820df790ba585cf7459b22ace47b94e
Th-Shivam <dreamyshivam01@gmail.com>
2025-02-13 16:44:20 +0530

    Calculates average of given three numbers

---
+++ avg_of_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a,b,c , avg ;
+    cout<<"Enter the first number : ";
+    cin>>a;
+    cout<<"Enter the second number : ";  
+    cin>>b;
+    cout<<"Enter the third number : ";   
+    cin>>c; 
+    avg = (a+b+c)/3;    
+    cout<<"The average of the three numbers is : "<<avg; 
+    return 0;
+};
