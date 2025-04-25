70e41cffe4f8fa9f339cbfd720db0686a79a1941
Shivam Singh <dreamyshivam01@gmail.com>
2025-04-25 23:37:40 +0530

    Update friend_fun.cpp

--- friend_fun.cpp
+++ friend_fun.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 using namespace std;
 
+// Class definition
 class A {
 private:
-    int num;
+    int num;  // Private member variable
 
 public:
+    // Constructor
     A(int n) : num(n) {}
 
-    // Friend function declaration
-    friend void showNumber(A obj);
+    // Setter method for updating the value
+    void setNumber(int n) {
+        num = n;
+    }
+
+    // Friend function declaration for accessing private data
+    friend void showNumber(const A& obj);
 };
 
 // Friend function definition
-void showNumber(A obj) {
+void showNumber(const A& obj) {
     cout << "The number is: " << obj.num << endl;
 }
 
 int main() {
-    A obj(10);
-    showNumber(obj); // Calling friend function
+    int input;
+    cout << "Enter a number: ";
+    cin >> input;
+
+    A obj(input);           // Creating object with user input
+    showNumber(obj);        // Calling friend function to display the value
+
+    // Optional: modifying the value using setter
+    obj.setNumber(input + 5);
+    cout << "After modifying using setter function:" << endl;
+    showNumber(obj);
+
     return 0;
 }
