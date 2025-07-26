d81faf9844edf4f39e2fa55327f8e1cdb9e4f83c
Th-Shivam <dreamyshivam01@gmail.com>
2025-07-26 23:54:41 +0530

    Quick sort

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
 using namespace std;
 
-class Sorter{
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-     public:
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
     
-     int n =6;
-     int pivot = 0;
-     int up = 1;
-     int down = n-1;
-     int arr[6];
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
 
-     void getInput(){
-            cout << "Enter the elements of the array: ";
-            for (int i = 0; i < n; i++)
-            {
-                cin >> arr[i];
-            }
-     }
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
 
-     void display(){
-            cout << "Sorted array: ";
-            for (int i = 0; i < n; i++)
-            {
-                cout << arr[i] << " ";
-            }
-            cout << endl;
-     }
-
-
-
-     void quickSort(){
-           
-     }
-
-} ;
-
-int main()
-{
-    Sorter sorter;
-    sorter.getInput();
-    sorter.quickSort();
-    sorter.display();
-
-
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    
+    quickSort(arr, 0, n - 1);
+    
+    cout << "\nSorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    
     return 0;
 }
